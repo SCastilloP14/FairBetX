e2cc5fe54b0d30e1fd81d592ea3d381162b11bfd
CJFuchs <135672705+CJFuchs@users.noreply.github.com>
2023-11-19 14:02:29 -0500

    Fixed login/registration card

--- static/login_script.js
+++ static/login_script.js
@@ -75,6 +75,7 @@
 
 
 document.addEventListener('DOMContentLoaded', function () {
+    // Login 
     var loginButton = document.getElementById("loginDropdownButton");
     var overlayDropdown = document.getElementById("overlayDropdown");
     var loginCardContainer = document.getElementById("loginCardContainer");
@@ -83,10 +84,23 @@ document.addEventListener('DOMContentLoaded', function () {
     var beingJourneyButton = document.getElementById("beingJourneyButton");
     var startBettingButton = document.getElementById("startBettingButton");
 
+    // Signup 
+    var newUserButton = document.getElementById("newUserButton");
+    var signUpOverlay = document.getElementById("signUpOverlay");
+    var signUpCardContainer = document.getElementById("signUpCardContainer");
+    var existingUserButton = document.getElementById("existingUserButton");
+
+    var registrationCancelButton = document.getElementById("registrationCancelButton");
+
 
     // Initially hide the loginDropdownContainer
     loginCardContainer.style.display = "none";
     overlayDropdown.style.display = "none";
+
+    signUpCardContainer.style.display = "none";
+    signUpOverlay.style.display = "none";
+    
+
 
     loginCancelButton.addEventListener('click', function ()  {
         closingDropdown();
@@ -105,6 +119,22 @@ document.addEventListener('DOMContentLoaded', function () {
         toggleDropdown();
     });
 
+    // Registration 
+    newUserButton.addEventListener('click', function ()  {
+        
+        closingDropdown();
+        toggleRegistrationDropdown();
+    });
+
+    existingUserButton.addEventListener('click', function () {
+        closeRegistrationDropdown();
+        toggleDropdown();
+    });
+
+    registrationCancelButton.addEventListener('click', function ()  {
+        closeRegistrationDropdown();
+        console.log("Clicked Registration Cancel");
+    });
 });
 
 function toggleDropdown() {
@@ -115,16 +145,35 @@ function toggleDropdown() {
 
     loginCardContainer.style.display = "block";
     overlayDropdown.style.display = "flex";
-
-
 }
 
 function closingDropdown() {
-    console.log("You pressed the overlay");
+    console.log("Closing Login Page");
+
     var loginCardContainer = document.getElementById("loginCardContainer");
     var overlayDropdown = document.getElementById("overlayDropdown");
 
     loginCardContainer.style.display = "none";
     overlayDropdown.style.display = "none";
 
+}
+
+function toggleRegistrationDropdown() {
+    console.log("Opening Registration Page");
+
+    var signUpCardContainer = document.getElementById("signUpCardContainer");
+    var signUpOverlay = document.getElementById("signUpOverlay");
+
+    signUpCardContainer.style.display = "block";
+    signUpOverlay.style.display = "flex";  
+}
+
+function closeRegistrationDropdown() {
+    console.log("Closing Registration Page");
+
+    var signUpCardContainer = document.getElementById("signUpCardContainer");
+    var signUpOverlay = document.getElementById("signUpOverlay");
+
+    signUpCardContainer.style.display = "none";
+    signUpOverlay.style.display = "none";  
 }
