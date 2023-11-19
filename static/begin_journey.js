b1ad30de5ace612c9e6e4b8f18d7dcd05af74307
CJFuchs <135672705+CJFuchs@users.noreply.github.com>
2023-11-19 12:17:46 -0500

    Fixed Login issues

--- static/begin_journey.js
+++ static/begin_journey.js
@@ -1,56 +1,103 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     var loginButton = document.getElementById("beginJourneyDropdownButton");
+//     var overlay = document.getElementById("beginJourneyOverlay");
+//     var loginContainer = document.getElementById("beginJourneyDropdownContainer");
+
+//     loginButton.addEventListener('click', function (event) {
+//         event.stopPropagation();
+//         toggleLoginDropdown();
+//     });
+
+//     overlay.addEventListener('click', function () {
+//         hideLoginDropdown();
+//     });
+
+//     document.addEventListener('click', function (event) {
+//         if (!loginContainer.contains(event.target) || isFormElement(event.target)) {
+//             hideLoginDropdown();
+//         }
+//     });
+
+//     // Add a click event listener to input elements to stop propagation
+//     var inputElements = document.querySelectorAll('#loginDropdownContainer input');
+//     inputElements.forEach(function (inputElement) {
+//         inputElement.addEventListener('click', function (event) {
+//             event.stopPropagation();
+//         });
+//     });
+
+//     function isFormElement(target) {
+//         return (
+//             target.tagName.toLowerCase() === "input" ||
+//             target.tagName.toLowerCase() === "button" ||
+//             (target.parentNode && isFormElement(target.parentNode))
+//         );
+//     }
+
+//     loginContainer.style.display = "none";
+//     overlay.style.display = "none";
+// });
+
+// function toggleLoginDropdown() {
+//     var loginContainer = document.getElementById("loginDropdownContainer");
+//     var overlay = document.getElementById("overlay");
+
+//     loginContainer.style.display = (loginContainer.style.display === "none" || loginContainer.style.display === "") ? "inline" : "none";
+//     overlay.style.display = (overlay.style.display === "none" || overlay.style.display === "") ? "block" : "none";
+// }
+
+// function hideLoginDropdown() {
+//     var loginContainer = document.getElementById("loginDropdownContainer");
+//     var overlay = document.getElementById("overlay");
+
+//     loginContainer.style.display = "none";
+//     overlay.style.display = "none";
+// }
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
-    var loginButton = document.getElementById("beginJourneyDropdownButton");
-    var overlay = document.getElementById("beginJourneyOverlay");
-    var loginContainer = document.getElementById("beginJourneyDropdownContainer");
+    var loginButton = document.getElementById("loginDropdownButton");
+    var overlayDropdown = document.getElementById("overlayDropdown");
+    var loginCardContainer = document.getElementById("loginCardContainer");
+    var loginCancelButton = document.getElementById("loginCancelButton");
 
-    loginButton.addEventListener('click', function (event) {
-        event.stopPropagation();
-        toggleLoginDropdown();
+
+    // Initially hide the loginDropdownContainer
+    loginCardContainer.style.display = "none";
+    overlayDropdown.style.display = "none";
+
+    loginCancelButton.addEventListener('click', function ()  {
+        closingDropdown();
+        console.log("Clicked Cancel");
     });
 
-    overlay.addEventListener('click', function () {
-        hideLoginDropdown();
+    loginButton.addEventListener('click', function () {
+        toggleDropdown();
     });
 
-    document.addEventListener('click', function (event) {
-        if (!loginContainer.contains(event.target) || isFormElement(event.target)) {
-            hideLoginDropdown();
-        }
-    });
+    
 
-    // Add a click event listener to input elements to stop propagation
-    var inputElements = document.querySelectorAll('#loginDropdownContainer input');
-    inputElements.forEach(function (inputElement) {
-        inputElement.addEventListener('click', function (event) {
-            event.stopPropagation();
-        });
-    });
-
-    function isFormElement(target) {
-        return (
-            target.tagName.toLowerCase() === "input" ||
-            target.tagName.toLowerCase() === "button" ||
-            (target.parentNode && isFormElement(target.parentNode))
-        );
-    }
-
-    loginContainer.style.display = "none";
-    overlay.style.display = "none";
 });
 
-function toggleLoginDropdown() {
-    var loginContainer = document.getElementById("loginDropdownContainer");
-    var overlay = document.getElementById("overlay");
+function toggleDropdown() {
+    console.log("Clicked Button");
 
-    loginContainer.style.display = (loginContainer.style.display === "none" || loginContainer.style.display === "") ? "inline" : "none";
-    overlay.style.display = (overlay.style.display === "none" || overlay.style.display === "") ? "block" : "none";
+    var loginCardContainer = document.getElementById("loginCardContainer");
+    var overlayDropdown = document.getElementById("overlayDropdown");
+
+    loginCardContainer.style.display = "block";
+    overlayDropdown.style.display = "flex";
+
+
 }
 
-function hideLoginDropdown() {
-    var loginContainer = document.getElementById("loginDropdownContainer");
-    var overlay = document.getElementById("overlay");
+function closingDropdown() {
+    console.log("You pressed the overlay");
+    var loginCardContainer = document.getElementById("loginCardContainer");
+    var overlayDropdown = document.getElementById("overlayDropdown");
 
-    loginContainer.style.display = "none";
-    overlay.style.display = "none";
+    loginCardContainer.style.display = "none";
+    overlayDropdown.style.display = "none";
+
 }
-
--- static/login_script.js
+++ static/login_script.js
@@ -1,56 +1,130 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     var loginButton = document.getElementById("loginDropdownButton");
+//     var overlay = document.getElementById("overlay");
+//     var loginContainer = document.getElementById("loginDropdownContainer");
+//     var newUser = document.getElementById("newUser");
+
+//     newUser.addEventListener('click', function () {
+//         console.log('Clicked newUser');
+//         hideLoginContainer();
+//     });
+
+//     loginButton.addEventListener('click', function (event) {
+//         event.stopPropagation();
+//         toggleLoginDropdown();
+//     });
+
+//     overlay.addEventListener('click', function () {
+//         hideLoginDropdown();
+//     });
+
+//     document.addEventListener('click', function (event) {
+//         if (!loginContainer.contains(event.target) || isFormElement(event.target)) {
+//             hideLoginDropdown();
+//         }
+//     });
+
+//     // Add a click event listener to input elements to stop propagation
+//     var inputElements = document.querySelectorAll('#loginDropdownContainer input');
+//     inputElements.forEach(function (inputElement) {
+//         inputElement.addEventListener('click', function (event) {
+//             event.stopPropagation();
+//         });
+//     });
+
+//     function isFormElement(target) {
+//     return (
+//         (target && target.tagName && target.tagName.toLowerCase() === "input") ||
+//         (target && target.tagName && target.tagName.toLowerCase() === "button") ||
+//         (target && target.parentNode && isFormElement(target.parentNode))
+//     );
+// }
+
+//     loginContainer.style.display = "none";
+//     overlay.style.display = "none";
+// });
+
+// function toggleLoginDropdown() {
+//     var loginContainer = document.getElementById("loginDropdownContainer");
+//     var overlay = document.getElementById("overlay");
+
+//     loginContainer.style.display = (loginContainer.style.display === "none" || loginContainer.style.display === "") ? "inline" : "none";
+//     overlay.style.display = (overlay.style.display === "none" || overlay.style.display === "") ? "block" : "none";
+// }
+
+// function hideLoginDropdown() {
+//     var loginContainer = document.getElementById("loginDropdownContainer");
+//     var overlay = document.getElementById("overlay");
+
+//     loginContainer.style.display = "none";
+//     overlay.style.display = "none";
+// }
+
+// function hideLoginContainer() {
+//     console.log('hideLoginContainer invoked');
+
+//     var loginContainer = document.getElementById("loginDropdownContainer");
+//     var overlay = document.getElementById("overlay");
+
+//     loginContainer.style.display = "none";
+//     overlay.style.display = "block";
+// }
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
     var loginButton = document.getElementById("loginDropdownButton");
-    var overlay = document.getElementById("overlay");
-    var loginContainer = document.getElementById("loginDropdownContainer");
+    var overlayDropdown = document.getElementById("overlayDropdown");
+    var loginCardContainer = document.getElementById("loginCardContainer");
+    var loginCancelButton = document.getElementById("loginCancelButton");
 
-    loginButton.addEventListener('click', function (event) {
-        event.stopPropagation();
-        toggleLoginDropdown();
+    var beingJourneyButton = document.getElementById("beingJourneyButton");
+    var startBettingButton = document.getElementById("startBettingButton");
+
+
+    // Initially hide the loginDropdownContainer
+    loginCardContainer.style.display = "none";
+    overlayDropdown.style.display = "none";
+
+    loginCancelButton.addEventListener('click', function ()  {
+        closingDropdown();
+        console.log("Clicked Cancel");
     });
 
-    overlay.addEventListener('click', function () {
-        hideLoginDropdown();
+    loginButton.addEventListener('click', function () {
+        toggleDropdown();
     });
 
-    document.addEventListener('click', function (event) {
-        if (!loginContainer.contains(event.target) || isFormElement(event.target)) {
-            hideLoginDropdown();
-        }
+    beingJourneyButton.addEventListener('click', function ()  {
+        toggleDropdown();
     });
 
-    // Add a click event listener to input elements to stop propagation
-    var inputElements = document.querySelectorAll('#loginDropdownContainer input');
-    inputElements.forEach(function (inputElement) {
-        inputElement.addEventListener('click', function (event) {
-            event.stopPropagation();
-        });
+    startBettingButton.addEventListener('click', function ()  {
+        toggleDropdown();
     });
 
-    function isFormElement(target) {
-        return (
-            target.tagName.toLowerCase() === "input" ||
-            target.tagName.toLowerCase() === "button" ||
-            (target.parentNode && isFormElement(target.parentNode))
-        );
-    }
-
-    loginContainer.style.display = "none";
-    overlay.style.display = "none";
 });
 
-function toggleLoginDropdown() {
-    var loginContainer = document.getElementById("loginDropdownContainer");
-    var overlay = document.getElementById("overlay");
+function toggleDropdown() {
+    console.log("Clicked Button");
 
-    loginContainer.style.display = (loginContainer.style.display === "none" || loginContainer.style.display === "") ? "inline" : "none";
-    overlay.style.display = (overlay.style.display === "none" || overlay.style.display === "") ? "block" : "none";
+    var loginCardContainer = document.getElementById("loginCardContainer");
+    var overlayDropdown = document.getElementById("overlayDropdown");
+
+    loginCardContainer.style.display = "block";
+    overlayDropdown.style.display = "flex";
+
+
 }
 
-function hideLoginDropdown() {
-    var loginContainer = document.getElementById("loginDropdownContainer");
-    var overlay = document.getElementById("overlay");
+function closingDropdown() {
+    console.log("You pressed the overlay");
+    var loginCardContainer = document.getElementById("loginCardContainer");
+    var overlayDropdown = document.getElementById("overlayDropdown");
 
-    loginContainer.style.display = "none";
-    overlay.style.display = "none";
+    loginCardContainer.style.display = "none";
+    overlayDropdown.style.display = "none";
+
 }
-
