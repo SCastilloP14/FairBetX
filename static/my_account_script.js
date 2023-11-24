d83a642f29db8769c7f98d1b639ecad2651e17cb
CJFuchs <135672705+CJFuchs@users.noreply.github.com>
2023-11-23 22:18:02 -0500

    added activity page

--- static/my_account_script.js
+++ static/my_account_script.js
@@ -4,23 +4,25 @@ document.addEventListener("DOMContentLoaded", function () {
   const personalSecurityTab = document.getElementById("personal-security-tab");
   // const cardInformationTab = document.getElementById("card-information-tab");
   const balanceTab = document.getElementById("balance-tab");
-  // const activityTab = document.getElementById("activity-tab");
+  const activityTab = document.getElementById("activity-tab");
 
     // Section 
   const personalInformationSection = document.getElementById("personal-information-section");
   const personalSecuritySection = document.getElementById("personal-security-section");
   // const cardInformationSection = document.getElementById("card-information-section");
   const balanceSection = document.getElementById("balance-section");
-  // const activitySection = document.getElementById("activity-section");
+  const activitySection = document.getElementById("activity-section");
+  const accountOverviewSection = document.getElementById("account-overview-section");
+  const accountPageSection = document.getElementById("account-page-section");
 
-
+  
   // Function to remove "active" class from all tabs
   function resetTabs() {
     personalInformationTab.classList.remove("active");
     personalSecurityTab.classList.remove("active");
     // cardInformationTab.classList.remove("active");
     balanceTab.classList.remove("active");
-    // activityTab.classList.remove("active");
+    activityTab.classList.remove("active");
   }
 
   // Show orders-section by default and hide positions-section
@@ -28,7 +30,9 @@ document.addEventListener("DOMContentLoaded", function () {
   personalSecuritySection.style.display = "none";
   // cardInformationSection.style.display = "none";
   balanceSection.style.display = "none";
-  // activitySection.style.display = "none";
+  activitySection.style.display = "none";
+  accountOverviewSection.style.display="block";
+
 
 
   // Add event listener to orders-tab
@@ -40,7 +44,9 @@ document.addEventListener("DOMContentLoaded", function () {
     personalSecuritySection.style.display = "none";
     // cardInformationSection.style.display = "none";
     balanceSection.style.display = "none";
-    // activitySection.style.display = "none";
+    activitySection.style.display = "none";
+    accountOverviewSection.style.display="block";
+
 
     // Highlight the clicked tab
     resetTabs();
@@ -56,7 +62,9 @@ document.addEventListener("DOMContentLoaded", function () {
     personalSecuritySection.style.display = "block";
     // cardInformationSection.style.display = "none";
     balanceSection.style.display = "none";
-    // activitySection.style.display = "none";
+    activitySection.style.display = "none";
+    accountOverviewSection.style.display="block";
+
 
     // Highlight the clicked tab
     resetTabs();
@@ -73,6 +81,8 @@ document.addEventListener("DOMContentLoaded", function () {
   //   cardInformationSection.style.display = "block";
   //   balanceSection.style.display = "none";
   //   // activitySection.style.display = "none";
+    // accountPageSection.style.display ='block';
+
 
   //   // Highlight the clicked tab
   //   resetTabs();
@@ -87,26 +97,31 @@ document.addEventListener("DOMContentLoaded", function () {
     personalSecuritySection.style.display = "none";
     // cardInformationSection.style.display = "none";
     balanceSection.style.display = "block";
-    // activitySection.style.display = "none";
+    activitySection.style.display = "none";
+    accountOverviewSection.style.display="block";
+
 
     // Highlight the clicked tab
     resetTabs();
     balanceTab.classList.add("active");
   });
 
-  // activityTab.addEventListener("click", function (event) {
-  //   event.preventDefault();
+  activityTab.addEventListener("click", function (event) {
+    event.preventDefault();
 
-  //   // Hide orders-section and positions-section and show fills-section
-  //   personalInformationSection.style.display = "none";
-  //   personalSecuritySection.style.display = "none";
-  //   cardInformationSection.style.display = "none";
-  //   balanceSection.style.display = "none";
-  //   activitySection.style.display = "block";
+    // Hide orders-section and positions-section and show fills-section
+    personalInformationSection.style.display = "none";
+    personalSecuritySection.style.display = "none";
+    // cardInformationSection.style.display = "none";
+    balanceSection.style.display = "none";
+    activitySection.style.display = "block";
+    accountOverviewSection.style.display="none";
+    accountPageSection.style.display ='block';
 
-  //   // Highlight the clicked tab
-  //   resetTabs();
-  //   activityTab.classList.add("active");
-  // });
+
+    // Highlight the clicked tab
+    resetTabs();
+    activityTab.classList.add("active");
+  });
 });
 
