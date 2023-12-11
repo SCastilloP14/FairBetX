0aad259450872db462bd4d76e673badf8d9a9b36
CJFuchs <135672705+CJFuchs@users.noreply.github.com>
2023-12-10 22:10:35 -0500

    Added League and Team pages

--- exchange_app/views.py
+++ exchange_app/views.py
@@ -284,7 +284,8 @@ class TeamDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         team = self.get_object()
         context["tickers"] = Ticker.objects.filter(ticker_game__game_home_team=team, ticker_status__in=["OPEN", "PARTIAL"]).order_by("-ticker_game__game_start_datetime").reverse()
-        context["teams"] = Team.objects.filter(team_league_1=league)
+        context["players"] = Player.objects.filter(player_team=team).order_by('-player_number').reverse()
+        context["games"] = Game.objects.filter(game_home_team = team)
         return context
 
 
