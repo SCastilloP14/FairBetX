c9e5b898b666464dabd0cd8d016179f16f2b2ca6
CJFuchs <135672705+CJFuchs@users.noreply.github.com>
2023-12-18 16:19:40 -0500

    Changes to Ticker List

--- static/JS/ticker_list.js
+++ static/JS/ticker_list.js
@@ -5,12 +5,35 @@ document.addEventListener('DOMContentLoaded', function () {
   // Add click event listener to each dropdown
   dropdowns.forEach(function (dropdown) {
     dropdown.addEventListener('click', function () {
+        console.log("Clicked Teams", dropdown)
+
       // Toggle the 'open' class for the clicked dropdown
       dropdown.classList.toggle('open');
     });
   });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+  // Get all elements with the class 'league-dropdown'
+  var leagueDropdowns = document.querySelectorAll('.league-dropdown');
+
+  // Add 'open' class to each league dropdown by default
+  leagueDropdowns.forEach(function (leagueDropdown) {
+    leagueDropdown.classList.add('open');
+  });
+
+  // Add click event listener to each league dropdown
+  leagueDropdowns.forEach(function (leagueDropdown) {
+    // Add a click event listener to each league dropdown
+    leagueDropdown.addEventListener('click', function (event) {
+      // Check if the click target is not part of the dropdown content
+      if (!leagueDropdown.contains(event.target.closest('.league-dropdown-content'))) {
+        // Toggle the 'open' class for the clicked league dropdown
+        leagueDropdown.classList.toggle('open');
+      }
+    });
+  });
+});
 
 document.addEventListener('DOMContentLoaded', function () {
   var filterButtons = document.querySelectorAll('.filterButton');
@@ -46,26 +69,28 @@ document.addEventListener('DOMContentLoaded', function () {
   var allEventsButton = document.getElementById('allEventsButton');
   var searchBarGames = document.getElementById('searchBarGames');
   var searchBarTeams = document.getElementById('searchBarTeams');
-  var sportsDropdown =  document.getElementById('sportsDropdown');
+  // var sportsDropdown =  document.getElementById('sportsDropdown');
+  var leagueDropdown = document.getElementById('leagueDropdown');
   // All tickers 
   var tickers = document.querySelectorAll('.ticker');
   var dropdowns = document.querySelectorAll('.custom-dropdown');
   var gameStatus = document.querySelectorAll('.gameStatus');
+  var leagueDropdowns = document.querySelectorAll('.league-dropdown');
 
   //Add event listener for sportsDropdown
-  sportsDropdown.addEventListener('change', function (event) {
-    event.preventDefault();
+  // sportsDropdown.addEventListener('change', function (event) {
+  //   event.preventDefault();
 
-    var selectedSport = sportsDropdown.value;
+  //   var selectedSport = sportsDropdown.value;
 
-    tickers.forEach(function(ticker) {
-        if (selectedSport === '' || ticker.classList.contains(selectedSport)) {
-            ticker.style.display = 'block';
-        } else {
-            ticker.style.display = 'none';
-        }
-    });
-  })
+  //   tickers.forEach(function(ticker) {
+  //       if (selectedSport === '' || ticker.classList.contains(selectedSport)) {
+  //           ticker.style.display = 'block';
+  //       } else {
+  //           ticker.style.display = 'none';
+  //       }
+  //   });
+  // })
 
 
   // Add event listener for allEventsButton
@@ -78,7 +103,8 @@ document.addEventListener('DOMContentLoaded', function () {
   // Add event listener for liveSportsButton
   liveSportsButton.addEventListener('click', function (event) {
       event.preventDefault();
-      filterTickers(gameStatus);
+      console.log("Pressedn live button");
+      filterLiveTickers(gameStatus);
   });
 
   
@@ -117,6 +143,19 @@ document.addEventListener('DOMContentLoaded', function () {
           // If gameStatus is null, show all tickers; otherwise, show/hide based on game status.
           ticker.style.display = gameStatus === null || status === "PLAYING" ? 'block' : 'none';
       });
+  }
+
+  function filterLiveTickers() {
+    var tickers = document.querySelectorAll('.ticker');
+    
+    tickers.forEach(function (ticker) {
+      var status = ticker.querySelector('.gameStatus').getAttribute('value');
+      var leagueDropdown = ticker.closest('.league-dropdown');
+
+      // Check if the league dropdown is open and the game is live
+      var showTicker = leagueDropdown.classList.contains('open') && status === "PLAYING";
+      ticker.style.display = showTicker ? 'block' : 'none';
+    });
   }
 
   // Function to filter all tickers by team names
